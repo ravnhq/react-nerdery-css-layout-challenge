57d9d8056406566c5181396cc1da5820765cc256
basurco eduardo <angelsoulsword@gmail.com>
2023-04-24 09:35:39 -0500

    feat: initial files

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="layout">
+      <div className="cell">
+        <div className="rectangle small"></div>
+        <ul>
+          <li>Home</li>
+          <li>Files</li>
+          <li>Recent Files</li>
+          <li>Shared Files</li>
+          <li>File Request</li>
+          <li>Trash</li>
+        </ul>
+      </div>
+      <div className="cell"></div>
+      <div className="cell">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        officia voluptatem magni repudiandae, repellat doloremque, commodi,
+        facere voluptatum ducimus esse perferendis amet nostrum sit dignissimos!
+        Tempore distinctio labore ipsum quos.
+      </div>
+    </div>
+  );
+}
+
+export default App;
