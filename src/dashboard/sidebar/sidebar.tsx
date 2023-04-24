c33b2f27611930e2cad4198c39346403379e385c
Alexander Apaza <alrus2797@gmail.com>
2023-04-24 18:23:22 -0500

    feat: add speech bubble menu component

--- src/dashboard/sidebar/sidebar.tsx
+++ src/dashboard/sidebar/sidebar.tsx
@@ -1,17 +1,21 @@
 import Button from "../../shared/ui/button";
+import SpeechBubbleMenu from "../../shared/ui/speech-bubble";
 import MenuList from "../menu-list/menu-list";
-import "./styles.css"
+import "./styles.css";
 
 const Sidebar = () => {
-
-  return <div className="sidebar">
-    <div className="gradient-bar"></div>
-    <MenuList />
-    <div className="create-section">
-      <Button>Create New</Button>
+  return (
+    <div className="sidebar">
+      <div className="gradient-bar"></div>
+      <MenuList />
+      <div className="sidebar-footer">
+        <SpeechBubbleMenu
+          items={["Upload Files", "Upload folder", "New folder", "More"]}
+        ></SpeechBubbleMenu>
+        <Button>Create New</Button>
+      </div>
     </div>
-  </div>
-}
-
+  );
+};
 
 export default Sidebar;
---
+++ src/shared/ui/speech-bubble/speech-bubble-menu.tsx
@@ -0,0 +1,26 @@
+import MenuItem from "../menu-item/menu-item";
+import "./style.css";
+
+interface SpeechBubbleMenuProps {
+  items: string[];
+  classNames?: string;
+  style?: React.CSSProperties;
+}
+
+const SpeechBubbleMenu = ({
+  items,
+  classNames,
+  style,
+}: SpeechBubbleMenuProps) => {
+  return (
+    <div className={`speech-bubble-menu ${classNames ?? ' '}`} style={style}>
+      <div className="menu-items">
+        {items.map((item, idx) => (
+          <MenuItem name={item} key={idx} gap={7}/> 
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SpeechBubbleMenu;
