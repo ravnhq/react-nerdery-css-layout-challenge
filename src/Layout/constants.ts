68563cafebcede6ff01db27f9999461ea035730b
Samuel Mundo <samuelmundo@ravn.co>
2023-04-25 10:37:02 -0600

    feat: adding color palette to TS

---
+++ src/Layout/constants.ts
@@ -0,0 +1,12 @@
+export const colors = {
+    DARKBLUE : '#343951',
+    DARKGRAY : '#858A9D',
+    ORANGE : '#ff9f00',
+    LIGHTGRAY : '#E0E7F1',
+    NAVYBLUE: '#689FF8',
+    GREEN: '#4AC29D',
+    RED: '#FF6860',
+    GRAY: '#F5F6FC',
+    GRAYVARIANT: '#464C61',
+    LIGHTORANGE: '#FFB233'
+}
