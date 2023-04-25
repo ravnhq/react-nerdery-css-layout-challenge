b5dd98d258bc048e6176f613b0d046a99177dea4
Alexander Apaza <alrus2797@gmail.com>
2023-04-25 12:47:46 -0500

    feat: change dashboard grid composition

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -1,12 +1,11 @@
+import HomeScreen from "./home-screen/Home"
 import Sidebar from "./sidebar/sidebar"
 import "./styles.css"
 
 const Dashboard = () => {
   return <div className="dashboard">
     <Sidebar />
-
-    <div style={{background: 'black', outline: '1px solid red'}}></div>
-    <div style={{background: 'black', outline: '1px solid red'}}></div>
+    <HomeScreen />
   </div>
 }
 
