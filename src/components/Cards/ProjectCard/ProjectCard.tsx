93e1493d5eb6e86a01576b0b6a5b9184181b1da5
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-25 09:00:29 -0600

    feat: add recently used section  on dashboard

--- src/components/Cards/ProjectCard/ProjectCard.tsx
+++ src/components/Cards/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,8 @@
-import { Ellipsis } from "../../Icons/Ellipsis";
-import BaseCard from "../BaseCard/BaseCard";
-import styles from './ProjectCard.module.css'
-
+import { ReactNode } from 'react';
+import { Ellipsis } from '../../Icons/Ellipsis';
+import BaseCard from '../BaseCard/BaseCard';
+import styles from './ProjectCard.module.css';
+import { Circle } from '../../Icons/Circle';
 
 interface Props {
     date: string;
@@ -14,16 +15,37 @@ const ProjectCard: React.FunctionComponent<Props> = ({
     name,
     circleCount,
 }) => {
+    const renderCircles = (): ReactNode => {
+        if (circleCount <= 2) {
+            return (
+                <div className={styles['circle-container']}>
+                    <Circle content={null} />
+                    <Circle content={null} />
+                </div>
+            );
+        }
 
-
+        return (
+            <div className={styles['circle-container']}>
+                <Circle content={null} />
+                <Circle content={null} />
+                <Circle content="+3" />
+            </div>
+        );
+    };
     return (
-    <BaseCard padding="padding-md">
-        <img src="/src/assets/svg/stackedRectangles.svg" alt="" />
-        <Ellipsis/>
-        <h4>{name}</h4>
-        <p>{`Created: ${date}`}</p>
-    </BaseCard>
-)
-}
+        <BaseCard padding="padding-md">
+            <div className={styles['card-header']}>
+                <span className={styles['card-subcontainer']}>
+                    <img src="/src/assets/svg/stackedRectangles.svg" alt="" />
+                    {renderCircles()}
+                </span>
+                <Ellipsis />
+            </div>
+            <h4 className={styles['card-title']}>{name}</h4>
+            <p className={styles['date-text']}>{`Created: ${date}`}</p>
+        </BaseCard>
+    );
+};
 
 export default ProjectCard;
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,51 +1,54 @@
-import { ProjectCard } from '../Cards/ProjectCard'
-import { EmptySquare } from '../EmptySquare'
-import { SectionHeader } from '../SectionHeader'
-import styles from './Dashboard.module.css'
+import { ProjectCard } from '../Cards/ProjectCard';
+import { EmptySquare } from '../EmptySquare';
+import { SectionHeader } from '../SectionHeader';
+import styles from './Dashboard.module.css';
 
 const projectData: projectItem[] = [
     {
         id: 1,
-        name: "App Project",
-        date: "20.02.2020",
-        circles: 2
+        name: 'App Project',
+        date: '20.02.2020',
+        circles: 2,
     },
     {
         id: 2,
-        name: "Project: fitbit",
-        date: "28.02.2020",
-        circles: 2
+        name: 'Project: fitbit',
+        date: '28.02.2020',
+        circles: 2,
     },
     {
         id: 3,
-        name: "Client Documents",
-        date: "4.03.2020",
-        circles: 3
+        name: 'Client Documents',
+        date: '4.03.2020',
+        circles: 3,
     },
-]
+];
 
 const Dashboard: React.FunctionComponent = () => (
-    <div className={styles["dashboard-main"]}>
-        <div className={styles["search-wrapper"]}>
-            <EmptySquare  borderColor="border-gray" />
-            <input type="text" placeholder='Search' />
+    <div className={styles['dashboard-main']}>
+        <div className={styles['search-wrapper']}>
+            <EmptySquare borderColor="border-gray" />
+            <input type="text" placeholder="Search" />
         </div>
         <section>
             <SectionHeader title="Recently Used">
-                <div className={styles["section-btn-container"]}>
-                    <EmptySquare  borderColor="border-black" />
-                    <EmptySquare  borderColor="border-gray" />
+                <div className={styles['section-btn-container']}>
+                    <EmptySquare borderColor="border-black" />
+                    <EmptySquare borderColor="border-gray" />
                 </div>
             </SectionHeader>
-            <div className={styles["card-container"]}>
-                {
-                    projectData.map((project) => (
-                        <ProjectCard key={project.id} date={project.date} name={project.name} circleCount={project.circles} />
-                    ))
-                }
+            <div className={styles['card-container']}>
+                {projectData.map((project) => (
+                    <ProjectCard
+                        key={project.id}
+                        date={project.date}
+                        name={project.name}
+                        circleCount={project.circles}
+                    />
+                ))}
             </div>
         </section>
     </div>
-)
+);
 
-export default Dashboard
+export default Dashboard;
