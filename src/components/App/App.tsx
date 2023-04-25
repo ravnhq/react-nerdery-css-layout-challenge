91dfc856848123757951dff223f63166fc2c6596
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-25 07:55:56 -0600

    feat: add initial dashboard component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,12 @@
 import { AsideNavBar } from '../AsideNavBar'
+import { Dashboard } from '../Dashboard'
 import { Layout } from '../Layout'
 import './App.module.css'
 
 const App = () => (
   <Layout>
     <AsideNavBar/>
-    <div className="">
-      <h1>Content</h1>
-    </div>
+    <Dashboard />
     <div className="">
       <h1>Graph</h1>
     </div>
