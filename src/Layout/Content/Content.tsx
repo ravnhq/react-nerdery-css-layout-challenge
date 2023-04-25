ce2f9c8dd98bcd0eb42c55d01b52702fb3acce20
Samuel Mundo <samuelmundo@ravn.co>
2023-04-25 10:39:19 -0600

    fix: reworked content and navbar logic

--- src/Layout/Content/Content.tsx
+++ src/Layout/Content/Content.tsx
@@ -1,62 +1,52 @@
-import { Circles, DSquares, Square } from '../Icons/Icons'
+import { Table } from './Table/Table'
+import { Box } from './Box/Box'
+import { SingleSquare, Square } from '../Icons/Icons'
+
 import styles from './content.module.css'
+import { colors } from '../constants'
 
 export const Content = () => {
   return (
   <section className={styles['content']}>
     <div className={styles['search-bar']}>
-      <Square color='#858A9D' w='25' h='25'/>
+      <Square color={colors.LIGHTGRAY} w='25' h='25'/>
       <input type="search" className={styles['search-bar-input']} name='search' id='search' placeholder='Search' />
     </div>
 
     <div className={styles['recently-used']}> 
-      <h1 className={styles['header']}>Recently Used</h1>
+      <div className={styles['section-header']}>
+        <h1 className={styles['header']}>Recently used</h1>
+        <label>
+          <SingleSquare />
+          <SingleSquare />
+        </label>
+      </div> 
       <div className={styles['box-group']}>
-        <div className={styles['box']}>
-          <div className={styles['box-icons']}>
-            <DSquares/>
-            <Circles/>
-          </div>
-          <div className={styles['box-title']}>
-            App Project
-          </div>
-          <div className={styles['box-date']}>
-            Created: 20.02.2020
-          </div>
-        </div>
-        <div className={styles['box']}>
-          <div className={styles['box-icons']}>
-            <DSquares/>
-            <Circles/>
-          </div>
-          <div className={styles['box-title']}>
-            App Project
-          </div>
-          <div className={styles['box-date']}>
-            Created: 20.02.2020
-          </div>
-        </div>
-        <div className={styles['box']}>
-          <div className={styles['box-icons']}>
-            <DSquares/>
-            <Circles/>
-          </div>
-          <div className={styles['box-title']}>
-            App Project
-          </div>
-          <div className={styles['box-date']}>
-            Created: 20.02.2020
-          </div>
-        </div>
+        <Box name='Project #1' date='14.04.2023' />
+        <Box name='Project #2' date='17.04.2023' />
+        <Box name='Project #3' date='20.04.2023' />
+        <Box name='Project #4' date='16.08.2023' />
       </div>
     </div>
 
     <div className={styles['recent-files']}> 
-      <h1 className={styles['header']}>Recent Files</h1>
+      <div className={styles['section-header']}>
+        <h1 className={styles['header']}>Recent files</h1>
+        <label>View All</label>
+      </div> 
+      <Table />
     </div>
 
-    <div className={styles['share-w-me']}> 
-      <h1 className={styles['header']}>Share with me</h1>
+    <div className={styles['share-w-me']}>
+      <div className={styles['section-header']}>
+        <h1 className={styles['header']}>Share with me </h1>
+        <label>View All</label>
+      </div> 
+      <div className={styles['box-group']}>
+        <Box name='Landing Page' date='14.04.2023' />
+        <Box name='Illustration Pack' date='17.04.2023' />
+        <Box name='CV Design' date='20.04.2023' />
+      </div>
     </div>
   </section>
   )
