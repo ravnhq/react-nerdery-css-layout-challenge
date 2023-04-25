b6637bfd6c2ea3f11c660ec98c17cde58522f58a
Samuel Mundo <samuelmundo@ravn.co>
2023-04-25 13:44:49 -0600

    feat: added logo and styles for primitives

--- src/Layout/Navbar/Navbar.tsx
+++ src/Layout/Navbar/Navbar.tsx
@@ -1,18 +1,47 @@
 import { Square } from '../Icons/Icons'
 import styles from './navbar.module.css'
+import primitives from '../Primitives/primitives.module.css'
 
 export const Navbar = () => {
   return (
-  <aside className={styles['navigation-bar']}>
-    <nav>
-      <ul>
-        <li className={styles['selected']}><span><Square/> Home</span></li>
-        <li><span><Square/> My Files</span></li>
-        <li><span><Square/> Recent Files</span></li>
-        <li><span><Square/> Shared Files</span></li>
-        <li><span><Square/> File Request</span></li>
-      </ul>
-    </nav>
-  </aside>
+    <>
+      <div className={styles['navigation-bar']}>
+        <nav>
+          <div className={styles['logo']}></div>
+          <ul className={styles['menu-bar']}>
+            <li><span><Square/> Home</span></li>
+            <li><span><Square/> My Files</span></li>
+            <li><span><Square/> Recent Files</span></li>
+            <li><span><Square/> Shared Files</span></li>
+            <li><span><Square/> File Request</span></li>
+            <li><span><Square/> Trash</span></li>
+
+          </ul>
+          <div className={styles['divider']}/>
+        </nav>
+        
+        <div className={styles['check-list-wrapper']}>
+          <div className={styles['check-list']}>
+            <div>
+              <input className={primitives['checkbox']} type="checkbox" name="uploadFiles" id="uploadFiles" />
+              <label htmlFor="uploadFiles">Upload Files</label>
+            </div>
+            <div>
+              <input className={primitives['checkbox']} type="checkbox" name="uploadFolder" id="uploadFolder" />
+              <label htmlFor="uploadFolder">Upload Folder</label>
+            </div>
+            <div>
+              <input className={primitives['checkbox']} type="checkbox" name="newFolder" id="newFolder" />
+              <label htmlFor="newFolder">New folder</label>
+            </div>
+            <div>
+              <input className={primitives['checkbox']} type="checkbox" name="more" id="more" />
+              <label htmlFor="more">More</label>
+            </div>
+          </div>
+          <button className={primitives['primary-button']}>Create New</button>
+        </div>
+      </div>
+  </>  
   )
 }
--- src/Layout/Widget/Widget.tsx
+++ src/Layout/Widget/Widget.tsx
@@ -1,5 +1,6 @@
 import { Graph, SingleSquare } from '../Icons/Icons'
 import styles from './widget.module.css'
+import primitives from '../Primitives/primitives.module.css'
 import { colors } from '../constants'
 
 export const Widget = () => {
@@ -18,28 +19,28 @@ export const Widget = () => {
         <div className={styles['graph-metric']}>
           <SingleSquare color={colors.ORANGE} rounded={true}/>
           <label className={styles['graph-metric-label']}>Documents <span>720 files</span></label>
-          <button>200GB</button>
+          <button className={primitives['secondary-button']}>200GB</button>
         </div>
         <div className={styles['divider']} />
 
         <div className={styles['graph-metric']}>
           <SingleSquare color={colors.NAVYBLUE} rounded={true}/>
           <label className={styles['graph-metric-label']}>Documents <span>720 files</span></label>
-          <button>125GB</button>
+          <button className={primitives['secondary-button']}>125GB</button>
         </div>
         <div className={styles['divider']} />
 
         <div className={styles['graph-metric']}>
           <SingleSquare color={colors.GREEN} rounded={true}/>
           <label className={styles['graph-metric-label']}>Documents <span>720 files</span></label>
-          <button>75GB</button>
+          <button className={primitives['secondary-button']}>75GB</button>
         </div>
         <div className={styles['divider']} />
 
         <div className={styles['graph-metric']}>
           <SingleSquare color={colors.DARKGRAY} rounded={true}/>
           <label className={styles['graph-metric-label']}>Documents <span>720 files</span></label>
-          <button>50GB</button>
+          <button className={primitives['secondary-button']}>50GB</button>
         </div>
       </div>
     </section>
