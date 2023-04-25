2c0a87e2b9178b7049c17f8046f677d3f9ef0b02
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-25 09:30:33 -0600

    refactor: Ellipsis component to have a more descriptive name

---
+++ src/components/Icons/EllipsisVertical/EllipsisVertical.tsx
@@ -0,0 +1,20 @@
+import styles from './Ellipsis.module.css';
+
+const EllipsisVertical: React.FunctionComponent = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="#c4c4c4"
+        className={styles['icon']}
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+        />
+    </svg>
+);
+
+export default EllipsisVertical;
