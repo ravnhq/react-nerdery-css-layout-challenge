2402685a713cf0068d0e2ae4648dde6ea54f7aa8
Alexander Apaza <alrus2797@gmail.com>
2023-04-25 12:47:06 -0500

    feat: add home screen

---
+++ src/dashboard/home-screen/Home.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const HomeScreen = () => {
+  return (
+    <div className="home-screen">
+      <div className="content"></div>
+      <div className="details"></div>
+    </div>
+  );
+};
+
+export default HomeScreen;
