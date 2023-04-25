ee3f0a0a3988417089e04a4eac8e72ca69950f46
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-25 07:44:17 -0600

    feat: Add Base Card Component

---
+++ src/components/Cards/BaseCard/BaseCard.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+import styles from './BaseCard.module.css'
+
+type paddingSize = "padding-sm" | "padding-md"
+
+interface Props extends PropsWithChildren {
+    padding: paddingSize
+}
+
+const BaseCard: React.FunctionComponent<Props> = ({ padding, children }) => (
+    <div className={`${styles["card"]} ${styles[padding]}`}>
+        {children}
+    </div>
+)
+
+export default BaseCard
