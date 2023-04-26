8b32379a9ed4157c2708edfe73b47fff2a204fb8
basurco eduardo <angelsoulsword@gmail.com>
2023-04-26 11:56:30 -0500

    feat: add roundedSquare

---
+++ src/roundedSquare/RoundedSquare.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RoundedSquare.css";
+
+interface RoundedSquare {
+  color: string;
+  size: string;
+}
+const RoundedSquare = () => {
+  return <div className="largeSquare default"></div>;
+};
+
+export default RoundedSquare;
