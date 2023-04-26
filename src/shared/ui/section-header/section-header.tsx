ade9ec31a8d62c7d740e48c79970c42575ac3654
Alexander Apaza <alrus2797@gmail.com>
2023-04-26 12:27:31 -0500

    feat: add header-section component
    This component is the first to use the customBoxModelInterface

---
+++ src/shared/ui/section-header/section-header.tsx
@@ -0,0 +1,18 @@
+import { CustomBoxModel } from '../../types/box-model-properties'
+import Flex from '../flex'
+import './style.css'
+
+interface SectionHeaderProps extends CustomBoxModel {
+  title: string,
+  append?: React.ReactNode
+  className?: string
+}
+
+const SectionHeader = ({className, append, title, boxProperties} : SectionHeaderProps) => {
+  return <Flex type='row' alignItems='center' justifyContent='space-between' className={className} boxProperties={boxProperties}>
+    <span className='header-title'>{title}</span>
+    {append}
+  </Flex>
+}
+
+export default SectionHeader
