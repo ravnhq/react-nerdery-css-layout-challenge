03d6682540c38a4020701bac1ffec18f27fdc542
basurco eduardo <angelsoulsword@gmail.com>
2023-04-26 10:10:45 -0500

    fix: changed card component changed card component to be 2 different components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import "./App.css";
+import Actions from "./actions/Actions";
+import Button from "./button/Button";
+import CardLargetrue from "./card/CardLargetrue";
+import CardSmalltrue from "./card/CardSmalltrue";
+import SectionHeader from "./header/SectionHeader";
+import Icon from "./icon/Icon";
+import RectangleSmall from "./rectangle/RectangleSmall";
+import SideBar from "./sideBar/SideBar";
 
 function App() {
   return (
     <div className="layout">
-      <div className="cell">
-        <div className="rectangle small"></div>
-        <ul>
-          <li>Home</li>
-          <li>Files</li>
-          <li>Recent Files</li>
-          <li>Shared Files</li>
-          <li>File Request</li>
-          <li>Trash</li>
-        </ul>
+      <div className="cell ">
+        <div className="sideBar">
+          <RectangleSmall />
+          <SideBar />
+        </div>
+        <div className="sideBarActions">
+          <Actions></Actions>
+          <Button icon="+">Create New</Button>
+        </div>
       </div>
-      <div className="cell"></div>
+      <div className="cell">
+        <div className="searchBar">
+          <Icon color="light-gray" /> <input type="text" placeholder="Search" />
+        </div>
+        <SectionHeader multipleActions={true}></SectionHeader>
+      </div>
       <div className="cell">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
         officia voluptatem magni repudiandae, repellat doloremque, commodi,
---
+++ src/card/types.ts
@@ -0,0 +1,4 @@
+export interface Card {
+  children: React.ReactNode;
+  footer: string;
+}
