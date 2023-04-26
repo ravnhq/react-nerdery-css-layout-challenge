e3e6751605547f2ebc33572c09f0f7e64edce029
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2023-04-26 13:40:17 -0600

    feat: added new class to base card component

--- src/components/Cards/BaseCard/BaseCard.tsx
+++ src/components/Cards/BaseCard/BaseCard.tsx
@@ -1,16 +1,14 @@
-import { PropsWithChildren } from "react";
-import styles from './BaseCard.module.css'
+import { PropsWithChildren } from 'react';
+import styles from './BaseCard.module.css';
 
-type paddingSize = "padding-sm" | "padding-md"
+type paddingSize = 'padding-sm' | 'padding-md' | 'padding-lg';
 
 interface Props extends PropsWithChildren {
-    padding: paddingSize
+    padding: paddingSize;
 }
 
 const BaseCard: React.FunctionComponent<Props> = ({ padding, children }) => (
-    <div className={`${styles["card"]} ${styles[padding]}`}>
-        {children}
-    </div>
-)
+    <div className={`${styles['card']} ${styles[padding]}`}>{children}</div>
+);
 
-export default BaseCard
+export default BaseCard;
