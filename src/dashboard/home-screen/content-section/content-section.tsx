ffaa309158ca8ebe147c893378c94ca8277ad4ff
Alexander Apaza <alrus2797@gmail.com>
2023-04-26 13:59:15 -0500

    feat: use icon-placeholder in content-section

--- src/dashboard/home-screen/content-section/content-section.tsx
+++ src/dashboard/home-screen/content-section/content-section.tsx
@@ -1,14 +1,15 @@
-import MenuIcon from '../../../shared/ui/menu-icon'
+import IconPlaceHolder from '../../../shared/ui/icon-placeholder'
 import SectionHeader from '../../../shared/ui/section-header'
 import TextField from '../../../shared/ui/text-field'
+import ContentSlickDots from '../content-slick-dots/content-slick-dots'
 import './styles.css'
 
 const ContentSection = () => {
   return <div className="content-section">
-    <TextField placeholder='Search' preppendIcon={<MenuIcon/>}/>
+    <TextField placeholder='Search' preppendIcon={<IconPlaceHolder/>}/>
     <SectionHeader title='Recently Used' boxProperties={{
       marginTop: 25
-    }} append={<div>Holis</div>} />
+    }} append={<ContentSlickDots />} />
   </div>
 }
 
---
+++ src/dashboard/home-screen/content-slick-dots/content-slick-dots.tsx
@@ -0,0 +1,14 @@
+import Flex from "../../../shared/ui/flex";
+import IconPlaceHolder from "../../../shared/ui/icon-placeholder";
+import "./styles.css";
+
+const ContentSlickDots = () => {
+  return (
+    <Flex type="row" gap={10}>
+      <IconPlaceHolder color="#343951" />
+      <IconPlaceHolder color="#9D9FAF" />
+    </Flex>
+  );
+};
+
+export default ContentSlickDots;
