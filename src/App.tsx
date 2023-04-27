f6913c665e50251163883f3aae4aefdf3156e007
basurco eduardo <angelsoulsword@gmail.com>
2023-04-26 19:22:12 -0500

    feat: add section component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Actions from "./actions/Actions";
 import Button from "./button/Button";
 import CardLarge from "./card/CardLarge";
 import CardSmall from "./card/CardSmall";
-import SectionHeader from "./header/SectionHeader";
 import Icon from "./icon/Icon";
 import RectangleSmall from "./rectangle/RectangleSmall";
+import Section from "./section/Section";
 import SideBar from "./sideBar/SideBar";
 
 const tableData = [
@@ -15,11 +15,64 @@ const tableData = [
   { name: "Dashboard Structure", members: 10, date: "Mar 9, 2020", type: 2 },
   { name: "Character Illustration", members: 3, date: "Mar 10, 2020", type: 0 },
 ];
-
-const Users = [
-  { avatar: "", id: 1, type: 1 },
-  { avatar: "", id: 1, type: 2 },
+const sharedFiles = [
+  {
+    name: "Landing Page",
+    date: "Created: 20.02.2020",
+    users: [
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 2 },
+    ],
+  },
+  {
+    name: "Illustration Pack",
+    date: "Created: 20.02.2020",
+    users: [
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 2 },
+      { avatar: "", id: 1, type: 0 },
+    ],
+  },
+  {
+    name: "CV Design",
+    date: "Created: 20.02.2020",
+    users: [
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 0 },
+    ],
+  },
 ];
+const files = [
+  {
+    name: "App Project",
+    date: "Created: 20.02.2020",
+    users: [
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+    ],
+  },
+  {
+    name: "Project: fitbit",
+    date: "Created: 28.02.2020",
+    users: [
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+    ],
+  },
+  {
+    name: "Client Documents",
+    date: "Created: 4.03.2020",
+    users: [
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+      { avatar: "", id: 1, type: 1 },
+    ],
+  },
+];
+
 function App() {
   return (
     <div className="layout">
@@ -37,14 +90,27 @@ function App() {
         <div className="searchBar">
           <Icon color="light-gray" /> <input type="text" placeholder="Search" />
         </div>
-        {/* <SectionHeader multipleActions={true}></SectionHeader>
-        <Table data={tableData}></Table> */}
-        <CardLarge numberUsers={Users} footer="footer">
-          Card
-        </CardLarge>
-        <CardSmall footer="footer" numberUsers={Users}>
-          Small
-        </CardSmall>
+        <Section title="Recently Used" action={true}>
+          <div style={{ display: "flex", gap: "14px", marginTop: "23px" }}>
+            {files.map((file) => (
+              <CardLarge footer={file.date} numberUsers={file.users}>
+                {file.name}
+              </CardLarge>
+            ))}
+          </div>
+        </Section>
+        {/* <Section title="Recent Files" action={false}>
+          <Table data={tableData}></Table>
+        </Section> */}
+        <Section title="Share with me" action={false}>
+          <div style={{ display: "flex", gap: "13px" }}>
+            {sharedFiles.map((file) => (
+              <CardSmall footer={file.date} numberUsers={file.users}>
+                {file.name}
+              </CardSmall>
+            ))}
+          </div>
+        </Section>
       </div>
       <div className="cell">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
--- src/header/SectionHeader.tsx
+++ src/header/SectionHeader.tsx
@@ -4,11 +4,12 @@ import Icon from "../icon/Icon";
 
 interface SectionHeader {
   multipleActions: boolean;
+  children: React.ReactNode;
 }
-const SectionHeader: FC<SectionHeader> = ({ multipleActions }) => {
+const SectionHeader: FC<SectionHeader> = ({ multipleActions, children }) => {
   return (
     <div className="header flex spaceBetween center">
-      <p className="titleHeader">Recent Files</p>
+      <p className="titleHeader">{children}</p>
       {multipleActions ? (
         <div className="headerActions">
           <Icon color="dark-blue" />
