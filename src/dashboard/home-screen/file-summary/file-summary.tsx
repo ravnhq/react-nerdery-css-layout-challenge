29c988602ed0490b38aa57a4809623037612102b
Alexander Apaza <alrus2797@gmail.com>
2023-04-28 13:05:20 -0500

    feat: add sections to file-summary component

--- src/dashboard/home-screen/file-summary/file-summary.tsx
+++ src/dashboard/home-screen/file-summary/file-summary.tsx
@@ -1,24 +1,19 @@
 import Flex from "../../../shared/ui/flex";
 import IconPlaceHolder from "../../../shared/ui/icon-placeholder";
-import SectionHeader from "../../../shared/ui/section-header";
-import Section from "../../../shared/ui/section/section";
 import TextField from "../../../shared/ui/text-field";
-import ContentSlickDots from "../content-slick-dots/content-slick-dots";
+import RecentlyUsedFilesSection from "../recently-used-files-section";
+import ShareWithMeSection from "../share-with-me-section";
+
 import "./styles.css";
 
 const FileSummary = () => {
-  const recenltyUsedSectionHeader = (
-    <SectionHeader
-      title="Recently Used"
-      append={<ContentSlickDots />}
-    />
-  );
 
   return (
     <div className="content-section">
       <TextField placeholder="Search" preppendIcon={<IconPlaceHolder />} />
       <Flex type="column" gap={38} boxProperties={{ marginTop: 25 }}>
-        <Section sectionHeader={recenltyUsedSectionHeader} sectionBody={<div>Holis</div>} contentSeparator={23}></Section>
+        <RecentlyUsedFilesSection />
+        <ShareWithMeSection />
       </Flex>
     </div>
   );
