b753bb9e058e06ae9189a3bf99a29f20ad7b61ab
Alexander Apaza <alrus2797@gmail.com>
2023-04-28 12:37:01 -0500

    feat: add users shared with component

---
+++ src/dashboard/home-screen/users-shared-with/users-shared=with.tsx
@@ -0,0 +1,28 @@
+import SharedUser from "../../../shared/types/user-shared-with";
+import "./styles.css";
+
+interface sharedWithProps {
+  users: SharedUser[];
+}
+
+const UsersSharedWith = ({ users }: sharedWithProps) => {
+  const firstUsers = users.slice(0, 3);
+  const firstUsersAvatars = firstUsers.map((user) => (
+    <div
+      className="shared-with"
+      style={{ border: `1px solid ${user.color ?? "#BEC0C8"}` }}
+      key={user.id}
+    ></div>
+  ));
+
+  if (users.length > 3)
+    firstUsersAvatars[2] = (
+      <div className="shared-with" key={3}>
+        <div>{`+${firstUsers.length - 1}`}</div>
+      </div>
+    );
+
+  return <>{firstUsersAvatars}</>;
+};
+
+export default UsersSharedWith;
