0d04b5707f1abd19fa8a2b3db8543eca494981f6
Alexander Apaza <alrus2797@gmail.com>
2023-05-01 14:13:52 -0500

    fix: add icon to sidebar button

--- src/dashboard/sidebar/sidebar.tsx
+++ src/dashboard/sidebar/sidebar.tsx
@@ -1,4 +1,5 @@
 import Button from "../../shared/ui/button";
+import IconPlaceHolder from "../../shared/ui/icon-placeholder";
 import SpeechBubbleMenu from "../../shared/ui/speech-bubble";
 import { GradientBar } from "../gradient-bar";
 import MenuList from "../menu-list/menu-list";
@@ -7,16 +8,35 @@ import "./styles.css";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <GradientBar width={74} height={21} boxProperties={{
-        margin: "36px 0px 51px 32px"
-      }} />
+      <GradientBar
+        width={74}
+        height={21}
+        boxProperties={{
+          margin: "36px 0px 51px 32px",
+        }}
+      />
       <MenuList />
       <div className="sidebar-footer">
         <SpeechBubbleMenu
           items={["Upload Files", "Upload folder", "New folder", "More"]}
           classNames="mb-20"
         ></SpeechBubbleMenu>
-        <Button>Create New</Button>
+        <Button>
+          Create New{" "}
+          <IconPlaceHolder
+            style={{
+              borderRadius: 4,
+              textAlign: "center",
+              verticalAlign: "middle",
+              padding: "3px"
+            }}
+            color="var(--light-orange)"
+            size={21}
+            filled
+          >
+            <p>+</p>
+          </IconPlaceHolder>
+        </Button>
       </div>
     </div>
   );
